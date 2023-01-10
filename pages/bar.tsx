a6df18217098a6095dd24c1560c50559f17fa011
Lukas Holzer <lukas.holzer@netlify.com>
2023-01-10 17:27:38 +0100

    chore: add reproduction

---
+++ pages/bar.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <main>
+      <h1>BAR</h1>
+    </main>
+  );
+};
---
+++ pages/foo.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <main>
+      <h1>FOO</h1>
+    </main>
+  );
+};
